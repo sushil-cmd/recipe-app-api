51453fad31dbc5783bd9b2126b5e308067f23d24
sushil-cmd <sushilswain33@gmail.com>
2021-07-25 12:21:44 +0530

    fixed flake8 error

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -4,36 +4,35 @@ from django.contrib.auth import get_user_model
 
 class ModelTests(TestCase):
 
-    def test_create_user_with_email_success(self):
-        """ Test creating a new user with email """
-        email = 'test@test.com'
-        password= 'Testpass123'
+    def test_create_user_with_email_successful(self):
+        """Test creating a new user with an email is successful"""
+        email = 'test@londonappdev.com'
+        password = 'Testpass123'
         user = get_user_model().objects.create_user(
             email=email,
-            password=password,
+            password=password
         )
+
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
-    
-    def test_newuser_email(self):
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        email = 'test@LONDONAPPDEV.COM'
+        user = get_user_model().objects.create_user(email, 'test123')
 
-        email = 'test@TEST.COM'
-        user = get_user_model().objects.create_user(email,'test123')
+        self.assertEqual(user.email, email.lower())
 
-        self.assertEqual(user.email,email.lower())
-    
     def test_newuser_invalid(self):
         """User must pass email"""
 
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user(None,'test123')
+            get_user_model().objects.create_user(None, 'test123')
 
-    
     def test_superuser_created(self):
         """test creating new superuser"""
-
-        user = get_user_model().objects.create_superuser('test@test.com','test123@')
-
+        user = get_user_model().objects.create_superuser(
+            'test@test.com',
+            'test123@')
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
